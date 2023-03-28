99d5a0e1de8c71f5dd16b278c62f4d8537d8d40d
Jake Zeisel <jacobzeisel@gmail.com>
2023-03-28 08:57:10 -0500

    make predictions and draw hands on video

--- project_main.py
+++ project_main.py
@@ -28,9 +28,32 @@ while True:
     # Read each frame from the camera
     _, frame = cap.read()
     x, y, c = frame.shape
-    
     # Flip the frame vertically
     frame = cv2.flip(frame, 1)
+
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # Get hand landmark prediction
+    result = hands.process(framergb)
+    # post process the result
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                # print(id, lm)
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+                landmarks.append([lmx,lmy])
+        print(landmarks)
+        mpDraw.draw_landmarks(frame, handslms, 
+            mpHands.HAND_CONNECTIONS)
+        # Predict gesture in Hand Gesture Recognition project
+        prediction = model.predict([landmarks])
+        classID = np.argmax(prediction)
+        className = classNames[classID]
+
+        # show the prediction on the frame
+        cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0,0,255), 2, cv2.LINE_AA)
     # Show the final output
     cv2.imshow("Output", frame)
     if cv2.waitKey(1) == ord('q'):
@@ -39,33 +62,3 @@ while True:
 # release the webcam and destroy all active windows
 cap.release()
 cv2.destroyAllWindows()
-
-framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-# Get hand landmark prediction
-result = hands.process(framergb)
-
-className = ''
-
-# post process the result
-if result.multi_hand_landmarks:
-    landmarks = []
-    for handslms in result.multi_hand_landmarks:
-        for lm in handslms.landmark:
-            # print(id, lm)
-            lmx = int(lm.x * x)
-            lmy = int(lm.y * y)
-            
-            landmarks.append([lmx, lmy])
-    
-    mpDraw.draw_landmarks(frame, handslms, 
-mpHands.HAND_CONNECTIONS)
-    
-# Predict gesture in Hand Gesture Recognition project
-prediction = model.predict([landmarks])
-print(prediction)
-classID = np.argmax(prediction)
-className = classNames[classID]
-
-# show the prediction on the frame
-cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
-               	1, (0,0,255), 2, cv2.LINE_AA)
